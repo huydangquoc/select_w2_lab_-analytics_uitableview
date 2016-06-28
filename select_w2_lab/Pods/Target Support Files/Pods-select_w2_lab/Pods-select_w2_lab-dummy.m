48bbaf8b93b38de0e2a026b968a08aca37fbad19
Dang Quoc Huy <huysamdua@yahoo.com>
2016-06-28 17:05:42 +0700

    add Pod files

---
+++ select_w2_lab/Pods/Target Support Files/Pods-select_w2_lab/Pods-select_w2_lab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_select_w2_lab : NSObject
+@end
+@implementation PodsDummy_Pods_select_w2_lab
+@end
---
+++ select_w2_lab/Pods/Target Support Files/Pods-select_w2_lab/Pods-select_w2_lab-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_select_w2_labVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_select_w2_labVersionString[];
+
